4dddb0e86a0ef33c9e7a81c84c6b6409169b7357
s-klimas <sebastianklimas@op.pl>
2026-03-02 19:00:55 +0100

    Initial commit

---
+++ src/main/java/pl/sebastianklimas/idp/IdPApplication.java
@@ -0,0 +1,13 @@
+package pl.sebastianklimas.idp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdPApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sebastianklimas/idp/IdPApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sebastianklimas.idp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
